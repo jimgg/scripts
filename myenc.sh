129092db98b28260714e9949c3c059b4a9da15e3
jimgg <jimgg2012@gmail.com>
2013-11-18 10:39:18 +0800

    myenc.sh: add salt

--- myenc.sh
+++ myenc.sh
@@ -1,7 +1,16 @@
 #!/Users/jim/Local/php/bin/php -q
 <?php
 
+$c = 0;
+if ($argc > 1) {
+    $c = $argv[1];
+}
 $str = file_get_contents('php://stdin');
 
-echo substr(base64_encode(sha1($str)), 0, 16);
+if ($c == 0) {
+    $salt = '';
+    echo substr(base64_encode(sha1($str . $salt)), 0, 16);
+} else {
+    echo base64_encode(sha1($str));
+}
 
